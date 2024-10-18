7842f86251b5cd2b3dc694921515b8a08d3b745b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-18 19:46:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿﻿int idade;
+
+Console.Write("Qual a sua idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 && idade <= 67)
+{
+    Console.WriteLine("Você pode ser doador de sangue.");
+}
+else
+{
+    Console.WriteLine("Você não pode ser doador de sangue.");
+}
